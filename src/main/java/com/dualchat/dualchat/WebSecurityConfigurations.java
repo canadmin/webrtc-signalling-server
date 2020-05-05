bc8f5135b1751ce4c980997ff8e3176f7f88f56f
canadmin <icanyard@gmail.com>
2020-05-05 21:42:41 +0300

    web sec conf

---
+++ src/main/java/com/dualchat/dualchat/WebSecurityConfigurations.java
@@ -0,0 +1,53 @@
+package com.dualchat.dualchat;
+
+import com.dualchat.dualchat.auth.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenFilter jwtTokenFilter;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configurePasswordEncoder(AuthenticationManagerBuilder builder) throws Exception{
+            builder.userDetailsService(userDetailsService).passwordEncoder(getBCryptPasswordEncoder());
+    }
+    @Bean
+    AuthenticationManager geAuthenticationManager() throws Exception{
+        return super.authenticationManagerBean();
+    }
+    @Bean
+    BCryptPasswordEncoder getBCryptPasswordEncoder(){
+       return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/signup","/login").permitAll().anyRequest()
+                .authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
